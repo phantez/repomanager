7b975a7c1598159d28de6c6342f894e311ccf24c
Stéphan Gorget <phantez@gmail.com>
2008-08-08 11:30:33 -0400

    Add licensing to the file headers

--- repos/views.py
+++ repos/views.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of RepoManager Project
+#
+# Copyright (C) 2008 RepoManager Project
+#
+# See AUTHORS for more informations
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from repomanager.repos.models import Repo
 from repomanager.repos.forms import NewRepositoryForm
 from django.contrib.auth.decorators import login_required
